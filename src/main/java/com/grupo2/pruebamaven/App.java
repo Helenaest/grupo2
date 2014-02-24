ab48663d79ef0e02e6d42084d1b25184738d580c
Helenaest <Helena@10.0.39.179>
2014-02-24 10:38:12 +0100

    creacion de proyecto

---
+++ src/main/java/com/grupo2/pruebamaven/App.java
@@ -0,0 +1,13 @@
+package com.grupo2.pruebamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
